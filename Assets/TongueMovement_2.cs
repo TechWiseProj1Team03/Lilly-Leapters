27fe8569d7f545563522e6b418e2ac3c5b509144
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2023-08-07 21:33:38 -0700

    TongueMovement_2 modified

--- Assets/TongueMovement_2.cs
+++ Assets/TongueMovement_2.cs
@@ -4,41 +4,60 @@ using UnityEngine;
 
 public class TongueMovement_2 : MonoBehaviour
 {
-    public float tongueLength = 3f;
-    public float interactionDistance = 2f;
-    public LayerMask interactableLayers;
-    
-    private LineRenderer lineRenderer;
-    private Vector2 tongueEnd;
+    public float tongueLength = 3f;  // Maximum length the tongue can extend
+    public float interactionDistance = 2f;  // Distance within which tongue can interact with objects
+    public LayerMask interactableLayers;  // Layers that tongue can interact with
+    private Animator tongueAnimator;  // Reference to the Animator component
+
+    private Vector2 tongueEnd;  // Calculated end point of the tongue
 
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.enabled = false;
+        // Initialize the tongueAnimator reference
+        tongueAnimator = GetComponent<Animator>();  // Fetch the Animator component on this GameObject
+        if (!tongueAnimator)  // Check if Animator component is present
+        {
+            Debug.LogError("No Animator component found on this GameObject!", this);  // Log error if Animator is missing
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T))  // Check for T key press (specific for second frog)
         {
-            lineRenderer.enabled = true;
-            tongueEnd = transform.position + transform.right * tongueLength;
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, tongueEnd);
+            HandleTongueExtension();  // Handle the tongue extension logic
+        }
+        else if (Input.GetKeyUp(KeyCode.T))  // Check for T key release (specific for second frog)
+        {
+            HandleTongueRetraction();  // Handle the tongue retraction logic
+        }
+    }
 
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, interactionDistance, interactableLayers);
-            if (hit.collider != null)
+    private void HandleTongueExtension()
+    {
+        if (tongueAnimator)  // Ensure Animator reference is not null
+        {
+            tongueAnimator.SetTrigger("ExtendTongue");  // Play tongue extension animation
+        }
+
+        tongueEnd = transform.position + transform.right * tongueLength;  // Calculate tongue's end position
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, interactionDistance, interactableLayers);  // Cast a ray to check for interactions
+        if (hit.collider != null)  // Check if ray hit any collider
+        {
+            Interactable interactable = hit.collider.GetComponent<Interactable>();  // Fetch the Interactable component of hit object
+            if (interactable != null)  // Ensure hit object has an Interactable component
             {
-                Interactable interactable = hit.collider.GetComponent<Interactable>();
-                if (interactable != null)
-                {
-                    interactable.Interact();
-                }
+                interactable.Interact();  // Trigger interaction
             }
         }
-        else if (Input.GetKeyUp(KeyCode.T))
+    }
+
+    private void HandleTongueRetraction()
+    {
+        if (tongueAnimator)  // Ensure Animator reference is not null
         {
-            lineRenderer.enabled = false;
+            tongueAnimator.SetTrigger("RetractTongue");  // Play tongue retraction animation
         }
     }
 }
