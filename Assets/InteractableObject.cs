ad6d1186d6589db1c4c42a8b81acb8a19cc537a0
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2023-08-07 21:20:54 -0700

    Fixed tongueMovement_1 for animation

---
+++ Assets/InteractableObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour, Interactable
+{
+
+    public FrogMovement frog;
+
+    public void Interact()
+    {
+        frog.CompletedTasks.Add(gameObject.name);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == frog.tongue.gameObject)
+        {
+            Interact();
+        }
+    }
+}
