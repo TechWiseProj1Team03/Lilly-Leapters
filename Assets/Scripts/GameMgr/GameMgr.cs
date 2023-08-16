ece51f88c675598c6758b5c74221a544d9e131be
Isaiah McLain <IsaiahMcLain@hotmail.com>
2023-08-15 17:54:03 -0700

    v2023.08.15 - Added timer management code to gamemgr

--- Assets/Scripts/GameMgr/GameMgr.cs
+++ Assets/Scripts/GameMgr/GameMgr.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class GameMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //  Singleton reference to our gamemgr 
+    public static GameMgr instance; 
+    
+    [Tooltip("This sets the timers start value, this needs to be seconds entered here.")]
+    public float timeSeconds; 
+
+    [Tooltip("Reference to our UI text in game, link UI prefab to this.")]
+    public Text time; 
+
+    //  Class variables for our different units of time 
+    private float minutes;  
+    private float seconds;
+    private float miliseconds; 
+
+    void Awake()
     {
-        
+        //  If we haven't instanciated our singleton then we do so 
+        if (instance == null) 
+        {
+            instance = this; 
+            DontDestroyOnLoad(gameObject); 
+        }
+        //  Otherwise we destory the gameObject for management purposes
+        else 
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeSeconds > 0) 
+        {
+           timeSeconds -= Time.deltaTime;
+           ConvertRemainderToTime(timeSeconds);
+        }
+        else
+        {
+            Debug.Log("time is out");
+        }
+    }
+
+    void ConvertRemainderToTime(float timeSeconds) 
+    {
+        timeSeconds += 1; 
+        minutes = Mathf.FloorToInt(timeSeconds / 60);
+        seconds = Mathf.FloorToInt(timeSeconds % 60); 
+        miliseconds = timeSeconds / 1000;
+        time.text = string.Format("{0:00}:{1:00}:{3:000}", minutes, seconds, miliseconds);
     }
 }
